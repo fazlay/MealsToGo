e54c683a57fd42893215f6a5fdc6f0a646e779aa
fazlay <rfazlay21@gmail.com>
2022-01-24 15:24:40 +0600

    icon

--- App.js
+++ App.js
@@ -7,18 +7,34 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SettingsScreen from "./features/restaurents/screen/SettingsScreen";
 import MapScreen from "./features/restaurents/screen/MapScreen";
-
+import { Ionicons } from "@expo/vector-icons";
 const Tab = createBottomTabNavigator();
 
+const TAB_ICON = {
+  Restaurent: "md-restaurant",
+  MapScreen: "md-map",
+  Settings: "md-settings",
+};
+
+const creatScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => {
+      <Ionicons name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  };
+};
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator screenOptions={creatScreenOptions}>
             <Tab.Screen name="Restaurent" component={RestaurentsScreen} />
+            <Tab.Screen name="MapScreen" component={MapScreen} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
-            <Tab.Screen name="Map Screen" component={MapScreen} />
           </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
